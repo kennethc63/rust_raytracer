0e7d62cefb32c0015087cbdbd10a91331007910d
Starfall63 <87585497+Starfall63@users.noreply.github.com>
2026-02-06 23:20:00 +0000

    cargo new project

---
+++ raytracer/target/debug/deps/raytracer-16001c51c7f370bd.d
@@ -0,0 +1,5 @@
+/Users/kenneth/Documents/repos/rust_raytracer/raytracer/target/debug/deps/libraytracer-16001c51c7f370bd.rmeta: src/main.rs
+
+/Users/kenneth/Documents/repos/rust_raytracer/raytracer/target/debug/deps/raytracer-16001c51c7f370bd.d: src/main.rs
+
+src/main.rs:
---
+++ raytracer/target/debug/deps/raytracer-8283276eb89c2584.d
@@ -0,0 +1,5 @@
+/Users/kenneth/Documents/repos/rust_raytracer/raytracer/target/debug/deps/libraytracer-8283276eb89c2584.rmeta: src/main.rs
+
+/Users/kenneth/Documents/repos/rust_raytracer/raytracer/target/debug/deps/raytracer-8283276eb89c2584.d: src/main.rs
+
+src/main.rs:
