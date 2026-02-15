57a4be74900c230fe46ccf7c1dd945afcc62d006
Starfall63 <87585497+Starfall63@users.noreply.github.com>
2026-02-15 13:33:18 +0000

    add world to the main.rs

--- raytracer/src/main.rs
+++ raytracer/src/main.rs
@@ -9,32 +9,20 @@ mod vec3;
 //Lets us use names locally
 use crate::{
     colour::*,
+    hittable::Hittable,
+    hittable_list::HittableList,
     ray::Ray,
+    sphere::Sphere,
     vec3::{Point3, Vec3, dot},
 };
 use std::io::stdout;
 
-fn hit_sphere(centre: Point3, radius: f64, r: &Ray) -> f64 {
-    let oc = centre - r.origin;
-    let a = r.direction.length_squared();
-    let h = dot(r.direction, oc);
-    let c = oc.length_squared() - radius * radius;
-    let discriminant = h * h - a * c;
-
-    if discriminant < 0.0 {
-        -1.0
-    } else {
-        (h - discriminant.sqrt()) / a
+fn ray_colour(r: &Ray, world: &impl Hittable) -> Colour {
+    if let Some(rec) = world.hit(r, 0.0, f64::INFINITY) {
+        return 0.5
+            * (Colour::new(rec.normal.x, rec.normal.y, rec.normal.z) + Colour::new(1.0, 1.0, 1.0));
     }
-}
-
-fn ray_colour(ray: &Ray) -> Colour {
-    let t = hit_sphere(Point3::new(0.0, 0.0, -1.0), 0.5, ray);
-    if t > 0.0 {
-        let N: Vec3 = (ray.at(t) - Vec3::new(0.0, 0.0, -1.0)).unit_vector();
-        return 0.5 * Colour::new(N.x + 1.0, N.y + 1.0, N.z + 1.0);
-    }
-    let unit_direction: Vec3 = ray.direction.unit_vector();
+    let unit_direction: Vec3 = r.direction.unit_vector();
     let a = 0.5 * (unit_direction.y + 1.0);
     (1.0 - a) * Colour::new(1.0, 1.0, 1.0) + a * Colour::new(0.5, 0.7, 1.0)
 }
@@ -48,6 +36,11 @@ fn main() {
     //Calculate image height
     let mut image_height = (image_width as f64 / aspect_ratio) as usize;
     image_height = if image_height < 1 { 1 } else { image_height };
+
+    // World
+    let mut world = HittableList::new();
+    world.add(Sphere::new(Point3::new(0.0, 0.0, -1.0), 0.5));
+    world.add(Sphere::new(Point3::new(0.0, -100.5, -1.0), 100.0));
 
     // Camera
     let focal_length = 1.0;
@@ -75,7 +68,7 @@ fn main() {
                 pixel00_loc + (i as f64 * pixel_delta_u) + (j as f64 * pixel_delta_v);
             let ray_direction = pixel_centre - camera_centre;
             let r = Ray::new(camera_centre, ray_direction);
-            let pixel_colour = ray_colour(&r);
+            let pixel_colour = ray_colour(&r, &world);
 
             write_colour(&mut out, pixel_colour);
         }
